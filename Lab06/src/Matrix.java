ec1a8fb2e51214da9d44af4bb963e30cbb98afe7
bcaudell95 <bcaudell95@gmail.com>
2015-03-24 13:47:19 -0500

    Matrix class

---
+++ Lab06/src/Matrix.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Matrix {
+	private int height, width;
+	private double[][] data;
+	
+	public Matrix(double[][] data) {
+		this.height = data.length;
+		this.width = data[0].length;
+		
+		this.data = data;
+		
+	}
+	
+	public Matrix(File csv) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(csv));
+		ArrayList<String> rows = new ArrayList<String>();
+		
+		while(br.ready()) rows.add(br.readLine());
+		this.data = new double[rows.size()][rows.get(0).split(",").length];
+		
+		this.height = this.data.length;
+		this.width = this.data[0].length;
+	}
+	
+	public void printMatrix() {
+		for(int i=0;i<this.height;i++) {
+			for(int j=0;j<this.width;j++) {
+				System.out.print(this.data[i][j] + ", ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
--- Lab06/src/main.java
+++ Lab06/src/main.java
@@ -2,7 +2,17 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Adam!");
+		
+		double[][] data = new double[3][2];
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<2;j++) {
+				data[i][j] = 2*i + j;
+			}
+		}
+		
+		Matrix tempMatrix = new Matrix(data);
+		tempMatrix.printMatrix();
+		
 	}
 	
 }
